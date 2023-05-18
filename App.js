aaab5b0cd2bf61df2d2534a4527316779a97d80f
YldzBerkay <berkay4421@gmail.com>
2023-05-18 23:34:03 +0300

    added new files

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import React, { useState } from 'react';
-import { NavigationContainer } from "@react-navigation/native";
-import RootNavigator from "./RootNavigator";
+import Onboarding from './screens/Onboarding'
+import Home from './screens/Home'
+import Profile from './screens/Profile'
+import {NavigationContainer} from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import {useEffect, useState} from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import {Alert} from 'react-native'
 
 export default function App() {
+  const [isOnboardingCompleted, setIsOnboardingCompleted] = useState(false)
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const info = await AsyncStorage.getItem('@onboarding')
+        const dt = JSON.parse(info)
+        setIsOnboardingCompleted(dt.isOnboardingCompleted)
+      } catch (e) {
+        // console.err(e)
+      }
+    })()
+  }, [])
+
+  const Stack = createNativeStackNavigator()
   return (
     <NavigationContainer>
-      <RootNavigator />
+      <Stack.Navigator
+        initialRouteName={ isOnboardingCompleted ? 'Home' : 'Onboarding'}
+      >
+          <Stack.Screen
+            name='Home'
+            component={Home}
+            option={{title: 'Home'}}
+          />
+          <Stack.Screen
+            name='Profile'
+            component={Profile}
+            option={{title: 'Profile'}}
+          />
+          <Stack.Screen
+            name='Onboarding'
+            component={Onboarding}
+            option={{title: 'Welcome'}}
+          />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
